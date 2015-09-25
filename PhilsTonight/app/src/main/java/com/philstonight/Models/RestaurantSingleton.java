f629e71db910bf9d73f316a172396f9807cfa457
Vishal Kuo <vfkuo@uwaterloo.ca>
2015-09-25 14:35:33 -0400

    Phil's Tonight?

--- PhilsTonight/app/src/main/java/com/philstonight/Models/RestaurantSingleton.java
+++ PhilsTonight/app/src/main/java/com/philstonight/Models/RestaurantSingleton.java
@@ -17,6 +17,7 @@ public class RestaurantSingleton {
             restaurants.add("Beta");
             restaurants.add("Night School");
             restaurants.add("Bomber");
+            restaurants.add("Study");
         }
         return restaurants;
     }
